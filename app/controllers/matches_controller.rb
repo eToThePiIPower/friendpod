ed2cd89d90eaa613d9760e9b57f8e4e1d540e60b
Charles Beynon <eToThePiIPower@gmail.com>
2017-05-13 21:03:09 -0400

    Show matches in web

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,11 @@ class MatchesController < ApplicationController
   acts_as_token_authentication_handler_for User
 
   def index
-    render json: current_user.find_related_interests
+    @matches = current_user.find_related_interests
+
+    respond_to do |format|
+      format.html { }
+      format.json { render json: @matches }
+    end
   end
 end
