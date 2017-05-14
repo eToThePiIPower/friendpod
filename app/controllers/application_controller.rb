a92dacef29fd6751f825930f374782f41d6edd60
Charles Beynon <eToThePiIPower@gmail.com>
2017-05-14 00:41:29 -0400

    Users can add phone numbers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :interest_list])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :interest_list])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :interest_list, :number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :interest_list, :number])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514015051) do
+ActiveRecord::Schema.define(version: 20170514043245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170514015051) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
+    t.string   "number"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
