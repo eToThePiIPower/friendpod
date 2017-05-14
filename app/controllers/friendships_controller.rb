041f8cf4910d468c34542935140c1073567dd9b9
Charles Beynon <eToThePiIPower@gmail.com>
2017-05-13 22:13:00 -0400

    Shows a match page on a mutual match

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,21 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find(params[:friend_id])
-    if current_user.requested_friends.include? friend
+    if current_user.friends.include? friend
+      render :show, friend_id: friend.id
+    elsif current_user.requested_friends.include? friend
       current_user.accept_request friend
+      render :show, friend_id: friend.id
     else
       current_user.friend_request friend
+      redirect_to matches_path
     end
-    redirect_to root_path
+  end
+
+  def show
+    friend = User.find(params[:friend_id]) 
+    if current_user.friends.include? friend
+      @friend = friend
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_ios_rails_for :users
 
   get 'matches', to: 'matches#index'
+
   post 'friends', to: 'friendships#create'
+  get 'friends', to: 'friendships#show'
 
   root to: 'static_pages#home'
 end
